ea305717d44a14f01609f3b1a1823ed906e810bb
Serpentariu13 <serpentarium13@mail.ru>
2024-08-19 00:02:04 +0300

    add redirect

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,11 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+definePageMeta({
+  middleware: [
+    function ({}) {
+      return navigateTo("/auth/login");
+    },
+  ],
+});
+</script>
 
 <template></template>
